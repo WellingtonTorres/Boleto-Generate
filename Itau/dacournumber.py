7bd56ef9e21fa48fafebfef676813c7e21b52277
Wellington Torres <51803361+WellingtonTorres@users.noreply.github.com>
2019-07-26 09:13:36 -0300

    Add files via upload

---
+++ Itau/dacournumber.py
@@ -0,0 +1,5 @@
+def geradac(nossonumero):
+    dac = 2
+    if nossonumero > 0:
+        dac = 0
+    return dac
---
+++ Itau/main.py
@@ -0,0 +1,14 @@
+import dacournumber as dac
+vCodbarra = 341
+vCodmoeda = 9
+dacCodbarra = '?'
+vFatorvencimento = 1667
+vValortitulo = 123.45 #são 10 digitos
+vCarteira = 110
+vNossonumero = 12345678 #8 digitos
+dacNossonumero = '?'
+vAgencia = '0057'
+vContacorrente = 12345
+dacAgconta = '?'
+
+print(dac.geradac(vNossonumero))
